5099fc1ccde3a1726b17d42298f270abd41d3ae7
Xin Liu <xhliu97@gmail.com>
2018-08-10 17:07:59 -0400

    added tiles to home

--- controllers/static.js
+++ controllers/static.js
@@ -1,5 +1,26 @@
+/*
+	All logic of static pages
+*/
+const db = require('../utils/db');
+const Soap = require('../models/Soap');
+
+/*
+	GET /
+	This page will render all buttons for quick checking out of soap
+*/
 exports.getHome = (req, res) => {
-	return res.render('static/home', {
-		title: 'Home',
+
+	return db.search('Soap', {})
+	.then(resp => {
+		const soap_data = resp.docs;
+
+		for(let i=0; i<soap_data.length; i++) {
+			soap_data[i].USD = (soap_data[i].price) / 100;
+		}
+
+		return res.render('static/home', {
+			title: 'Home',
+			soaps: soap_data
+		})
 	})
 }
